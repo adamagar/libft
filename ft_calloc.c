e80b8a98fbf6d4247b5ec3dc66874a03baa62d7c
Abdullah Agar <aagar@c3r10p1.42quebec.com>
2023-01-13 19:34:57 -0500

    fixed bugs

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,18 +6,19 @@
 /*   By: aagar <aagar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 18:57:04 by aagar             #+#    #+#             */
-/*   Updated: 2023/01/13 19:30:50 by aagar            ###   ########.fr       */
+/*   Updated: 2023/01/13 19:34:08 by aagar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h";
 
-void    *calloc(size_t count, size_t size)
+void	*calloc(size_t count, size_t size)
 {
-    void *p;
-    p = malloc(size * count);
-    if (!p)
-    return (NULL);
-    ft_bzero(p, count);
-    return (p);
+	void *p;
+
+	p = malloc(size * count);
+	if (!p)
+		return (NULL);
+	ft_bzero(p, count);
+	return (p);
 }
