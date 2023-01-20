6ec7a248822d91db26b551caec1acdba3f8aa438
Abdullah Agar <adamagar88@gmail.com>
2023-01-20 17:28:56 -0500

    improvements

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: aagar <aagar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 18:30:40 by aagar             #+#    #+#             */
-/*   Updated: 2023/01/20 17:10:24 by aagar            ###   ########.fr       */
+/*   Updated: 2023/01/20 17:27:13 by aagar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memset(void *b, int c, size_t len)
 	p = (unsigned char *)b;
 	while (len > 0)
 	{
-		p[len - 1] = unsigned c;
+		p[len - 1] = (unsigned char)c;
 		len--;
 	}
 	return (b);
